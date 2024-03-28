ffd0a4dd2706fb14f17d6d262aa166ed81f3b90f
superdragonwolf <Janik.Hinsberger.95@gmail.com>
2024-03-28 13:03:59 +0100

    reset to master code. changing this section breaks the game

--- modinfo.lua
+++ modinfo.lua
@@ -1,9 +1,9 @@
 return {
-  name = 'Beyond All Reason - Tactics',
-  description = 'Beyond All Reason Tactictical Defense Mod',
-  shortName = 'BYAR T',   -- 'BAR' is used by original bar project still
+  name = 'Beyond All Reason',
+  description = 'Beyond All Reason',
+  shortName = 'BYAR',   -- 'BAR' is used by original bar project still
   version = '$VERSION',
-  mutator = 'Tactical Defence Mutator',
+  mutator = 'Official',
   game = 'Beyond All Reason',
   shortGame = 'BYAR',
   modtype = 1,
