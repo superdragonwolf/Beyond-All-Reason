460534ff8b6d457906db68950d4504c3a040b7ef
superdragonwolf <Janik.Hinsberger.95@gmail.com>
2024-04-05 16:17:51 +0200

    spawning defending Cortex units spawning attacking Armada waves destroying cortex units before each round

--- singleplayer/scenarios/scenarioscripts/tacticsscenarioscript.lua
+++ singleplayer/scenarios/scenarioscripts/tacticsscenarioscript.lua
@@ -2,31 +2,95 @@ if not gadgetHandler:IsSyncedCode() then
 	return
 end
 
--- west rot = 32767, 18785, 32287, 13024, 15314, 45314, 130240
-
-local wave = {
-    {name = "cordemon", x = 1533, y = 385, z = 4300, rot = 2, neutral = false, teamID = 0, queue = {
-    {cmdID = CMD.FIGHT, position = {px = 1533, py = 385, pz = 1479}},
-    }},
-    {name = "cordemon", x = 1733, y = 385, z = 4300, rot = 2, neutral = false, teamID = 0, queue = {
-    {cmdID = CMD.FIGHT, position = {px = 1733, py = 385, pz = 1479}},
-    }},
-}
-
-local defender = {
+local guards = {
     {name = "cordemon", x = 1533, y = 385, z = 4400, rot = 2, neutral = false, teamID = 0},
     {name = "cordemon", x = 1733, y = 385, z = 4400, rot = 2, neutral = false, teamID = 0},
 }
 
-local function SpawnDefender() -- spawns titans at one end of the map and commands them to move to the other end
-    for k , unit in pairs(defender) do
+local wave_1 = {
+    {name = "armflea", x = 1533, y = 385, z = 2550, rot = 0, neutral = false, teamID = 1, queue = {
+    {cmdID = CMD.FIGHT, position = {px = 1533, py = 385, pz = 4600}},
+    }},
+    {name = "armflea", x = 1583, y = 385, z = 2550, rot = 0, neutral = false, teamID = 1, queue = {
+        {cmdID = CMD.FIGHT, position = {px = 1583, py = 385, pz = 4600}},
+    }},
+    {name = "armflea", x = 1633, y = 385, z = 2550, rot = 0, neutral = false, teamID = 1, queue = {
+    {cmdID = CMD.FIGHT, position = {px = 1633, py = 385, pz = 4600}},
+    }},
+    {name = "armflea", x = 1683, y = 385, z = 2550, rot = 0, neutral = false, teamID = 1, queue = {
+        {cmdID = CMD.FIGHT, position = {px = 1683, py = 385, pz = 4600}},
+    }},
+    {name = "armflea", x = 1733, y = 385, z = 2550, rot = 0, neutral = false, teamID = 1, queue = {
+        {cmdID = CMD.FIGHT, position = {px = 1733, py = 385, pz = 4600}},
+    }},
+    {name = "armflea", x = 1533, y = 385, z = 2600, rot = 0, neutral = false, teamID = 1, queue = {
+    {cmdID = CMD.FIGHT, position = {px = 1533, py = 385, pz = 4600}},
+    }},
+    {name = "armflea", x = 1583, y = 385, z = 2600, rot = 0, neutral = false, teamID = 1, queue = {
+        {cmdID = CMD.FIGHT, position = {px = 1583, py = 385, pz = 4600}},
+    }},
+    {name = "armflea", x = 1633, y = 385, z = 2600, rot = 0, neutral = false, teamID = 1, queue = {
+    {cmdID = CMD.FIGHT, position = {px = 1633, py = 385, pz = 4600}},
+    }},
+    {name = "armflea", x = 1683, y = 385, z = 2600, rot = 0, neutral = false, teamID = 1, queue = {
+        {cmdID = CMD.FIGHT, position = {px = 1683, py = 385, pz = 4600}},
+    }},
+    {name = "armflea", x = 1733, y = 385, z = 2600, rot = 0, neutral = false, teamID = 1, queue = {
+        {cmdID = CMD.FIGHT, position = {px = 1733, py = 385, pz = 4600}},
+    }},
+    {name = "armflea", x = 1533, y = 385, z = 2650, rot = 0, neutral = false, teamID = 1, queue = {
+    {cmdID = CMD.FIGHT, position = {px = 1533, py = 385, pz = 4600}},
+    }},
+    {name = "armflea", x = 1583, y = 385, z = 2650, rot = 0, neutral = false, teamID = 1, queue = {
+        {cmdID = CMD.FIGHT, position = {px = 1583, py = 385, pz = 4600}},
+    }},
+    {name = "armflea", x = 1633, y = 385, z = 2650, rot = 0, neutral = false, teamID = 1, queue = {
+    {cmdID = CMD.FIGHT, position = {px = 1633, py = 385, pz = 4600}},
+    }},
+    {name = "armflea", x = 1683, y = 385, z = 2650, rot = 0, neutral = false, teamID = 1, queue = {
+        {cmdID = CMD.FIGHT, position = {px = 1683, py = 385, pz = 4600}},
+    }},
+    {name = "armflea", x = 1733, y = 385, z = 2650, rot = 0, neutral = false, teamID = 1, queue = {
+        {cmdID = CMD.FIGHT, position = {px = 1733, py = 385, pz = 4600}},
+    }},
+    {name = "armpw", x = 1533, y = 385, z = 2700, rot = 0, neutral = false, teamID = 1, queue = {
+    {cmdID = CMD.FIGHT, position = {px = 1533, py = 385, pz = 4600}},
+    }},
+    {name = "armflea", x = 1583, y = 385, z = 2700, rot = 0, neutral = false, teamID = 1, queue = {
+        {cmdID = CMD.FIGHT, position = {px = 1583, py = 385, pz = 4600}},
+    }},
+    {name = "armflea", x = 1633, y = 385, z = 2700, rot = 0, neutral = false, teamID = 1, queue = {
+    {cmdID = CMD.FIGHT, position = {px = 1633, py = 385, pz = 4600}},
+    }},
+    {name = "armflea", x = 1683, y = 385, z = 2700, rot = 0, neutral = false, teamID = 1, queue = {
+        {cmdID = CMD.FIGHT, position = {px = 1683, py = 385, pz = 4600}},
+    }},
+    {name = "armpw", x = 1733, y = 385, z = 2700, rot = 0, neutral = false, teamID = 1, queue = {
+        {cmdID = CMD.FIGHT, position = {px = 1733, py = 385, pz = 4600}},
+    }},
+}
+
+local defender_1 = { 
+    {name = "corak", x = 1583, y = 385, z = 3600, rot = 2, neutral = false, teamID = 0},
+    {name = "corak", x = 1633, y = 385, z = 3600, rot = 2, neutral = false, teamID = 0},
+    {name = "corak", x = 1683, y = 385, z = 3600, rot = 2, neutral = false, teamID = 0},
+    {name = "corak", x = 1733, y = 385, z = 3600, rot = 2, neutral = false, teamID = 0},
+    {name = "corak", x = 1533, y = 385, z = 3650, rot = 2, neutral = false, teamID = 0},
+    {name = "corak", x = 1583, y = 385, z = 3650, rot = 2, neutral = false, teamID = 0},
+    {name = "corak", x = 1633, y = 385, z = 3650, rot = 2, neutral = false, teamID = 0},
+    {name = "corak", x = 1683, y = 385, z = 3650, rot = 2, neutral = false, teamID = 0},
+    {name = "corak", x = 1733, y = 385, z = 3650, rot = 2, neutral = false, teamID = 0},
+}
+
+local function SpawnDefender(units) -- spawns defending units
+    for k , unit in pairs(units) do
         if UnitDefNames[unit.name] then
             Spring.CreateUnit(unit.name, unit.x, unit.y, unit.z, unit.rot, unit.teamID)
         end
     end
 end
 
-local function SpawnWave() -- spawns titans at one end of the map and commands them to move to the other end
+local function SpawnAndSendWave(wave) -- spawns attacking units order the command
     for k , unit in pairs(wave) do
         if UnitDefNames[unit.name] then
             local unitID = Spring.CreateUnit(unit.name, unit.x, unit.y, unit.z, unit.rot, unit.teamID)
@@ -39,16 +103,35 @@ local function SpawnWave() -- spawns titans at one end of the map and commands t
     end
 end
 
+function DeletePlayerBots() -- remove grunts, thugs and aggravators of the player
+    local playerunits = Spring.GetTeamUnits(0)
+    for k, unitID in ipairs(playerunits) do
+        local unitDefID = Spring.GetUnitDefID(unitID)
+        if unitDefID then
+            local name = UnitDefs[unitDefID].name
+            if name == "corak" or name == "corthud" or name == "corstorm" then
+                Spring.Echo(UnitDefs[unitDefID].name .. " removed")
+                Spring.DestroyUnit(unitID, false, false)
+            end
+        end
+    end
+end
+
 function gadget:GameFrame(frameNum) --fires off every frame
     local n = frameNum
         if n == 10 then
-            SpawnDefender()
-            SpawnWave()
+            SpawnDefender(guards)
+            SpawnDefender(defender_1)
+            SpawnAndSendWave(wave_1)
         end
-        if n == 1800 then -- 1min 
-            SpawnWave()
+        if n == 1800 then -- 1min
+            DeletePlayerBots()
+            SpawnDefender(defender_1)
+            SpawnAndSendWave(wave_1)
         end
-        if n == 3600 then -- 2min 
-            SpawnWave()
+        if n == 3600 then -- 2min
+            DeletePlayerBots()
+            SpawnDefender(defender_1)
+            SpawnAndSendWave(wave_1)
         end
 end
